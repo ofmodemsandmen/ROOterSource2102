269fb93c34c0572c86e008f51a805f3e0a471a53
ROOter <ralph@rfactory.org>
2021-12-14 13:30:58 -0800

    More WG209
    More WG209

--- package/rooter/0routerspecfic/wg209/files/usr/lib/rooter/modem-led.sh
+++ package/rooter/0routerspecfic/wg209/files/usr/lib/rooter/modem-led.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh 
 
 log() {
 	logger -t "modem-led " "$@"
@@ -9,43 +9,68 @@ COMMD=$2
 
 	case $COMMD in
 		"0" )
-			echo none > /sys/class/leds/green:globe/trigger
-			echo 0  > /sys/class/leds/green:globe/brightness
-			echo none > /sys/class/leds/green:signal/trigger
-			echo 0  > /sys/class/leds/green:signal/brightness
+			echo none > /sys/class/leds/green:signal1/trigger
+			echo 0  > /sys/class/leds/green:signal1/brightness
+			echo none > /sys/class/leds/green:signal2/trigger
+			echo 0  > /sys/class/leds/green:signal2/brightness
+			echo none > /sys/class/leds/green:signal3/trigger
+			echo 0  > /sys/class/leds/green:signal3/brightness
+			echo none > /sys/class/leds/green:internet/trigger
+			echo 0  > /sys/class/leds/green:internet/brightness
 			;;
 		"1" )
-			echo timer > /sys/class/leds/green:globe/trigger
-			echo 500  > /sys/class/leds/green:globe/delay_on
-			echo 500  > /sys/class/leds/green:globe/delay_off
+			echo timer > /sys/class/leds/green:internet/trigger
+			echo 500  > /sys/class/leds/green:internet/delay_on
+			echo 500  > /sys/class/leds/green:internet/delay_off
 			;;
 		"2" )
-			echo timer > /sys/class/leds/green:globe/trigger
-			echo 200  > /sys/class/leds/green:globe/delay_on
-			echo 200  > /sys/class/leds/green:globe/delay_off
+			echo timer > /sys/class/leds/green:internet/trigger
+			echo 200  > /sys/class/leds/green:internet/delay_on
+			echo 200  > /sys/class/leds/green:internet/delay_off
 			;;
 		"3" )
-			echo timer > /sys/class/leds/green:globe/trigger
-			echo 1000  > /sys/class/leds/green:globe/delay_on
-			echo 0  > /sys/class/leds/green:globe/delay_off
+			echo timer > /sys/class/leds/green:internet/trigger
+			echo 1000  > /sys/class/leds/green:internet/delay_on
+			echo 0  > /sys/class/leds/green:internet/delay_off
 			;;
 		"4" )
-			echo none > /sys/class/leds/green:globe/trigger
-			echo 1  > /sys/class/leds/green:globe/brightness
 			sig2=$3
-			echo timer > /sys/class/leds/green:signal/trigger
-			if [ $sig2 -lt 18 -a $sig2 -gt 0 ] 2>/dev/null;then
-				echo 500  > /sys/class/leds/green:signal/delay_on
-				echo 500  > /sys/class/leds/green:signal/delay_off
-			elif [ $sig2 -ge 18 -a $sig2 -lt 31 ] 2>/dev/null;then
-				echo 150  > /sys/class/leds/green:signal/delay_on
-				echo 150  > /sys/class/leds/green:signal/delay_off
-			elif [ $sig2 -eq 31 ] 2>/dev/null;then
-				echo 0  > /sys/class/leds/green:signal/delay_on
-				echo 1000  > /sys/class/leds/green:signal/delay_off
-			else
-				echo 950  > /sys/class/leds/green:signal/delay_on
-				echo 950  > /sys/class/leds/green:signal/delay_off
+			if [ $sig2 -lt 8 -a $sig2 -gt 0 ] 2>/dev/null;then
+				echo none > /sys/class/leds/green:internet/trigger
+				echo 1  > /sys/class/leds/green:internet/brightness
+				echo none > /sys/class/leds/green:signal3/trigger
+				echo 0  > /sys/class/leds/green:signal3/brightness
+				echo none > /sys/class/leds/green:signal2/trigger
+				echo 0  > /sys/class/leds/green:signal2/brightness
+				echo none > /sys/class/leds/green:signal1/trigger
+				echo 0  > /sys/class/leds/green:signal1/brightness
+			elif [ $sig2 -ge 8 -a $sig2 -lt 16 ] 2>/dev/null;then
+				echo none > /sys/class/leds/green:internet/trigger
+				echo 1  > /sys/class/leds/green:internet/brightness
+				echo none > /sys/class/leds/green:signal3/trigger
+				echo 1  > /sys/class/leds/green:signal3/brightness
+				echo none > /sys/class/leds/green:signal2/trigger
+				echo 0  > /sys/class/leds/green:signal2/brightness
+				echo none > /sys/class/leds/green:signal1/trigger
+				echo 0  > /sys/class/leds/green:signal1/brightness
+			elif [ $sig2 -ge 16 -a $sig2 -lt 24 ] 2>/dev/null;then
+				echo none > /sys/class/leds/green:internet/trigger
+				echo 1  > /sys/class/leds/green:internet/brightness
+				echo none > /sys/class/leds/green:signal3/trigger
+				echo 1  > /sys/class/leds/green:signal3/brightness
+				echo none > /sys/class/leds/green:signal2/trigger
+				echo 1  > /sys/class/leds/green:signal2/brightness
+				echo none > /sys/class/leds/green:signal1/trigger
+				echo 0  > /sys/class/leds/green:signal1/brightness
+			elif [ $sig2 -ge 24 ] 2>/dev/null;then
+				echo none > /sys/class/leds/green:internet/trigger
+				echo 1  > /sys/class/leds/green:internet/brightness
+				echo none > /sys/class/leds/green:signal3/trigger
+				echo 1  > /sys/class/leds/green:signal3/brightness
+				echo none > /sys/class/leds/green:signal2/trigger
+				echo 1  > /sys/class/leds/green:signal2/brightness
+				echo none > /sys/class/leds/green:signal1/trigger
+				echo 1  > /sys/class/leds/green:signal1/brightness
 			fi
 			;;
 	esac
