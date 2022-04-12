6a3119ff925cc988a9a8ce79b9a0dc841e6488a6
ROOter <ralph@rfactory.org>
2022-04-12 11:45:34 -0700

    more translation
    more translation

---
+++ package/rooter/0optionalapps/pingtest/files/usr/lib/lua/luci/controller/ping.lua
@@ -0,0 +1,12 @@
+-- Licensed to the public under the Apache License 2.0.
+
+module("luci.controller.ping", package.seeall)
+
+local I18N = require "luci.i18n"
+local translate = I18N.translate
+
+function index()
+	local page
+	page = entry({"admin", "modem", "ping"}, cbi("ping"), _(translate("Custom Ping Test")), 45)
+	page.dependent = true
+end
