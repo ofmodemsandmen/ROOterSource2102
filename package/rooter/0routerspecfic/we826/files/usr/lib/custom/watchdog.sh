0ecdb9bcf299bcea4964ffaeeddfb930da0c9b33
Dairyman <ralph@rfactory.org>
2023-10-03 16:21:20 -0700

    we826wd
    we826wd

--- package/rooter/0routerspecfic/we826/files/usr/lib/custom/watchdog.sh
+++ package/rooter/0routerspecfic/we826/files/usr/lib/custom/watchdog.sh
@@ -2,13 +2,13 @@
 
 #for example WE825-Q watchdog gpio is 2
 # WE826 is 11
-wd_gpio="11"
+wd_gpio="491"
 
 echo $wd_gpio > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio$wd_gpio/direction
-echo 14 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio14/direction
-echo 255 >/sys/class/gpio/gpio14/value
+echo 494 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio494/direction
+echo 255 >/sys/class/gpio/gpio494/value
 
 while [ 1 ]
 do
